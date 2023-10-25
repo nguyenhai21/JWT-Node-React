eb94eb989299d0dba6104ce4bad41155a04676f1
hai <tienhai2182001@gmail.com>
2023-10-25 12:52:22 +0700

    init

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,6 +1,9 @@
 // get the client
 import mysql from 'mysql2';
 
+import bcrypt from 'bcryptjs';
+const salt = bcrypt.genSaltSync(10);
+
 // create the connection to database
 const connection = mysql.createConnection({
     host: 'localhost',
@@ -23,7 +26,11 @@ const handleNewCreateUser = (req, res) => {
     // let username = req.body.username;
     let { email, password, username } = req.body;
 
+    let hashPassword = bcrypt.hashSync(password, salt);
+    console.log('>>>check hash password: ', hashPassword)
 
+    let check = bcrypt.compareSync(password, hashPassword); // true
+    console.log('>>>check password: ', check)
 
     connection.query(
         `INSERT INTO users (email, password, username)
@@ -32,7 +39,7 @@ const handleNewCreateUser = (req, res) => {
             if (err) {
                 console.log(err)
             }
-            console.log(results)
+            console.log('success')
         }
     );
     return res.send('handleNewCreateUser');
