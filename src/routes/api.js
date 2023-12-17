bc3e526739f1e8af1674afdd79b3b8dfdd2980cd
hai <tienhai2182001@gmail.com>
2023-12-17 11:13:31 +0700

    init

--- src/routes/api.js
+++ src/routes/api.js
@@ -13,6 +13,7 @@ const initApiRoutes = (app) => {
 
     router.get('/test-api', apiController.testApi);
     router.post('/register', apiController.handleRegister);
+    router.post('/login', apiController.handleLogin);
 
     return app.use("/api/v1/", router);
 }
