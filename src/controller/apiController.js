0d8938ec0cc64e10a0d6328053f979e38d90098c
hai <tienhai2182001@gmail.com>
2023-12-18 12:50:00 +0700

    init

--- src/controller/apiController.js
+++ src/controller/apiController.js
@@ -45,11 +45,23 @@ const handleRegister = async (req, res) => {
 }
 
 const handleLogin = async (req, res) => {
-    console.log('>>check login from react', req.body)
-    return res.status(200).json({
-        message: 'ok',
-        data: 'test api login'
-    })
+    try {
+
+        let data = await loginRegisterService.handleUserLogin(req.body);
+
+        return res.status(200).json({
+            EM: data.EM,//error mesage
+            EC: data.EC, //error code
+            DT: data.DT, //data
+        })
+    } catch (error) {
+        console.log('>>check err: ', error)
+        return res.status(500).json({
+            EM: 'Error from server',//error mesage
+            EC: '-1', //error code
+            DT: '', //data
+        })
+    }
 }
 
 module.exports = {
--- src/service/loginRegisterService.js
+++ src/service/loginRegisterService.js
@@ -1,6 +1,8 @@
 // import { where } from "sequelize"
 import db from "../models/models/index"
 import bcrypt from 'bcryptjs';
+import { raw } from "body-parser";
+import { Op } from "sequelize";
 
 const salt = bcrypt.genSaltSync(10);
 
@@ -71,6 +73,58 @@ const registerNewUser = async (rawUserData) => {
         }
     }
 }
+
+const checkPassword = (inputPassword, hashPassword) => {
+    return bcrypt.compareSync(inputPassword, hashPassword); // true or false
+}
+
+const handleUserLogin = async (rawData) => {
+    try {
+        let user = await db.User.findOne({
+            where: {
+                [Op.or]: [
+                    { email: rawData.valueLogin },
+                    { phone: rawData.valueLogin }
+                ]
+            }
+        })
+        if (user) {
+            console.log('>>>found user with email/phone')
+            let isCorrectPassword = checkPassword(rawData.password, user.password)
+            if (isCorrectPassword === true) {
+                return {
+                    EM: 'OK',
+                    EC: 0,
+                    DT: ''
+                }
+            }
+        }
+        console.log('>>Input user with email/phone', rawData.valueLogin, `passsword: `, rawData.password);
+        return {
+            EM: 'Your email/phone number or password  is incorrect! ',
+            EC: 1,
+            DT: ''
+        }
+
+
+        // let isPhoneExist = await checkPhoneExist(rawUserData.phone);
+        // if (isPhoneExist === true) {
+        //     return {
+        //         EM: 'The phone number is already exist',
+        //         EC: 1,
+        //         DT: ''
+        //     }
+        // }
+    } catch (error) {
+        console.log('>>check error:', error)
+        return {
+            EM: 'Somethong wrongs in service...',
+            EC: -2
+        }
+    }
+
+}
+
 module.exports = {
-    registerNewUser
+    registerNewUser, handleUserLogin
 }
