1dcd316fd4086862504326ea70093ee98a774f2d
hai <tienhai2182001@gmail.com>
2023-12-27 22:39:49 +0700

    init

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,19 +1,62 @@
 import userApiService from '../service/userApiService';
 
-const readFunc = (req, res) => {
+const readFunc = async (req, res) => {
 
+    try {
+        let data = await userApiService.getAllUser();
+
+        return res.status(200).json({
+            EM: data.EM,//error mesage
+            EC: data.EC, //error code
+            DT: data.DT, //data
+        })
+    } catch (error) {
+        console.log('>>check err: ', error)
+        return res.status(500).json({
+            EM: 'Error from server',//error mesage
+            EC: '-1', //error code
+            DT: '', //data
+        })
+    }
 }
 
 const createFunc = (req, res) => {
+    try {
 
+    } catch (error) {
+        console.log('>>check err: ', error)
+        return res.status(500).json({
+            EM: 'Error from server',//error mesage
+            EC: '-1', //error code
+            DT: '', //data
+        })
+    }
 }
 
 const updateFunc = (req, res) => {
+    try {
 
+    } catch (error) {
+        console.log('>>check err: ', error)
+        return res.status(500).json({
+            EM: 'Error from server',//error mesage
+            EC: '-1', //error code
+            DT: '', //data
+        })
+    }
 }
 
 const deleteFunc = (req, res) => {
+    try {
 
+    } catch (error) {
+        console.log('>>check err: ', error)
+        return res.status(500).json({
+            EM: 'Error from server',//error mesage
+            EC: '-1', //error code
+            DT: '', //data
+        })
+    }
 }
 
 module.exports = {
--- src/service/userApiService.js
+++ src/service/userApiService.js
@@ -1,19 +1,72 @@
+import { where } from "sequelize";
 import db from "../models/models/index";
 
-const getAllUser = () => {
-
+const getAllUser = async () => {
+    try {
+        let users = await db.User.findAll({
+            attributes: ['id', 'username', 'email', 'phone', 'sex'],
+            include: { model: db.Group, attributes: ['name', 'description'] },
+        });
+        if (users) {
+            return {
+                EM: 'get succes',
+                EC: 0,
+                DT: users
+            }
+        } else {
+            return {
+                EM: 'get succes',
+                EC: 0,
+                DT: []
+            }
+        }
+    } catch (error) {
+        console.log(error)
+        return {
+            EM: 'something wrongs wwiths server',
+            EC: 1,
+            DT: []
+        }
+    }
 };
 
-const createNewUser = () => {
+const createNewUser = async (data) => {
+    try {
+        await db.User.create({
 
+        })
+    } catch (error) {
+        console.log(error);
+    }
 };
 
-const updateUser = () => {
+const updateUser = async (data) => {
+    try {
+        let user = await db.User.findOne({
+            where: { id: data.id }
+        })
 
+        if (user) {
+            //update
+            user.save({
+
+            })
+        } else {
+            //not found
+        }
+    } catch (error) {
+        console.log(error);
+    }
 };
 
-const deleteUser = () => {
-
+const deleteUser = async (id) => {
+    try {
+        await db.User.delete({
+            where: { id: id }
+        })
+    } catch (error) {
+        console.log(error);
+    }
 };
 
 module.exports = {
